8fb3344ddea615df1efccb8c5f6045b936e5b6ae
Mozgin Dmitry <m0391n@gmail.com>
2011-08-29 19:41:42 +0400

    First commit

---
+++ src/com/m039/tools/mqst/InstantSMSActivity.java
@@ -0,0 +1,15 @@
+package com.m039.tools.mqst;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class InstantSMSActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
